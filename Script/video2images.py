78b976a271c00f8ac1aa4b82c4d8d66f6620a66d
Bhanu Prasad M <mbhanuprsd@gmail.com>
2022-01-05 22:39:14 +0530

    Implemented green screen video with ue background

--- Script/video2images.py
+++ Script/video2images.py
@@ -5,12 +5,43 @@ import os
 from tkinter import Tk     # from tkinter import Tk for Python 3.x
 from tkinter.filedialog import askopenfilename
 
+def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
+    # initialize the dimensions of the image to be resized and
+    # grab the image size
+    dim = None
+    (h, w) = image.shape[:2]
+
+    # if both the width and height are None, then return the
+    # original image
+    if width is None and height is None:
+        return image
+
+    # check to see if the width is None
+    if width is None:
+        # calculate the ratio of the height and construct the
+        # dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
+
+    # otherwise, the height is None
+    else:
+        # calculate the ratio of the width and construct the
+        # dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    # resize the image
+    resized = cv2.resize(image, dim, interpolation = inter)
+
+    # return the resized image
+    return resized
+
 Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
 filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
   
 # Read the video from specified path
 cam = cv2.VideoCapture(filename)
-  
+
 try:
       
     # creating a folder named data
@@ -36,12 +67,14 @@ while(True):
   
     if ret:
         # if video is still left continue creating images
-        name = current_dir + '/' + folder_name + '/' + str(currentframe).zfill(8) + '.jpg'
-        print ('Creating...' + name)
-  
+        name = current_dir + '/' + folder_name + '/' + str(currentframe).zfill(8) + '.png'
+        print ('Compressing...' + name)
+
+        frame = image_resize(frame, width=1280)
         # writing the extracted images
+        print ('Creating...' + name)
         cv2.imwrite(name, frame)
-  
+
         # increasing counter so that it will
         # show how many frames are created
         currentframe += 1
